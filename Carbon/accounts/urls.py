adbe4d09e1a6a7edca88c5028cbf830aafc1cd08
Bhavya Grover <bhavya2526@gmail.com>
2023-05-17 21:45:30 +0530

    fifth commit

--- Carbon/accounts/urls.py
+++ Carbon/accounts/urls.py
@@ -6,19 +6,19 @@ from .views import *
 urlpatterns = [
   
     path('login/',views.login,name="login"),
-    path('',views.home),
+    path('',views.home, name='home'),
     path('calculatetransport/',views.calculatetransport),
     path('calculate/',views.calculate),
-    path('request/',views.request),
+    path('request/',views.request, name='request'),
     #path('login/',views.login),
     path('calculatehouse/',views.calculatehouse),
-    path('option/',views.option),
+    path('option/',views.option, name='option'),
     path('monitor/',views.monitor),
-    path('reduces/',views.reduces),
-    path('individual/',views.individual),
-    path('organisation/',views.organisation),
+    path('reduces/',views.reduces, name='reduces'),
+    path('individual/',views.individual, name='individual'),
+    path('organisation/',views.organisation, name='organisation'),
     path('leaderboard/',views.leaderboard),
-    path('howitworks/',views.howitworks),
+    path('howitworks/',views.howitworks, name='howitworks'),
 ]   
 
 
